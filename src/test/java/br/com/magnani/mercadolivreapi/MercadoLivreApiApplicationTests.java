0ddb0178155b86639169ca691828723552d6e955
Raquel Magnani <quel.magnani88@gmail.com>
2021-02-15 19:47:34 -0300

    first commit

---
+++ src/test/java/br/com/magnani/mercadolivreapi/MercadoLivreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magnani.mercadolivreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
