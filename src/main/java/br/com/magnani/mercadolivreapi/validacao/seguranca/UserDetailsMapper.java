6b345ef5de71a412eb82ee390c0a8eb1869b418b
Raquel Magnani <quel.magnani88@gmail.com>
2021-03-06 14:22:40 -0300

    Revert "implementa seguranca"
    This reverts commit a37f1bc7356cd09240992764b9ad47828aa5fa02.

--- src/main/java/br/com/magnani/mercadolivreapi/validacao/seguranca/UserDetailsMapper.java
+++
@@ -1,8 +0,0 @@
-package br.com.magnani.mercadolivreapi.validacao.seguranca;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailsMapper {
-
-	UserDetails map(Object shouldBeASystemUser);
-}
